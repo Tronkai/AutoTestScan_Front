a61c2d23bf97fa5785cbb9e1485262b1805315b6
Tronkai <kerwin.zhu@tron.network>
2020-12-01 17:17:12 +0800

    Change login

--- cypress/integration/examples/Login.js
+++ cypress/integration/examples/Login.js
@@ -2,7 +2,7 @@ describe('连接钱包', () => {
     it('导入私钥钱包', () => {
         cy.visit('https://tronscan.org/#/walletconnect/importWallet')
         cy.get('.ant-input').type('01')
-        cy.get('.ant-btn').click()
+        cy.waitUntil(() => cy.get('.ant-btn')).click()
 
         cy.get('.nav-static > .nav-link').invoke('text').then( address => {
             expect("TMVQGm1qAQYVdetCeGRRkTWYYrLXuHK2HC").to.equal(address)
