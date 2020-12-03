f5aebb669f831f0b54d09bda79ebb2eb6b203f22
Tronkai <kerwin.zhu@tron.network>
2020-12-03 14:37:35 +0800

    Representatives

--- cypress/integration/examples/Home.js
+++ cypress/integration/examples/Home.js
@@ -1,12 +1,12 @@
-describe('Tronscan首页', () => {
-    it('上边栏数据校验!', () => {
-        cy.visit('https://tronscan.org')
-        cy.get(':nth-child(1) > :nth-child(1) > .hash-body > .text-left > .produced-by > .ml-2').click()
-        let a
-        cy.get('.table > tbody > :nth-child(3) > td').invoke('text').then(value => {
-            a = value
-            cy.url().should('include','/block/'+a)
-            cy.url().should('include','/blocks/')
-        })
-    })
-})
+// describe('Tronscan首页', () => {
+//     it('上边栏数据校验!', () => {
+//         cy.visit('https://tronscan.org')
+//         cy.get(':nth-child(1) > :nth-child(1) > .hash-body > .text-left > .produced-by > .ml-2').click()
+//         let a
+//         cy.get('.table > tbody > :nth-child(3) > td').invoke('text').then(value => {
+//             a = value
+//             cy.url().should('include','/block/'+a)
+//             cy.url().should('include','/blocks/')
+//         })
+//     })
+// })
