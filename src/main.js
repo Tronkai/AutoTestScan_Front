d6c10bc3fa04e32d29ce530ec837e979629802b0
Tron Kai <kerwin.zhu@tron.network>
2023-02-10 11:54:33 +0800

    change ip

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './plugins/element.js'
 import axios from 'axios';
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
-axios.defaults.baseURL = 'http://127.0.0.1:5000';
+axios.defaults.baseURL = 'https://trontest.com:5000';
 new Vue({
   router,
   render: h => h(App)
